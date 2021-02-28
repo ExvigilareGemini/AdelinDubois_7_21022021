d14d7ed275de566e90840eba97f6cd5b1cd22be3
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-02-28 22:43:32 +0100

    filtering dropdown content with dropdown input

--- controller/app.js
+++ controller/app.js
@@ -80,7 +80,7 @@ function dropdownHTMLGenerator(key) {
           <form class="dropdown-menu dropdown-large bg-${colorsOfDropwdown[countForColorsOfDropdown]} text-white" aria-labelledby="dropdownMenuButton">
               <div class="d-flex flex-wrap" data-category="${key}" data-color="${colorsOfDropwdown[countForColorsOfDropdown]}">
                   ${objectOfArraysForDropdown[key].map((el) => `
-                  <a class="w-50 dropdown-item text-truncate" href="#">${el}</a>
+                  <a class="w-50 dropdown-item text-truncate" data-category="${key}" data-content="${el.toLowerCase()}" href="#">${el}</a>
                   `).join('')}
               </div>
           </form>
@@ -271,4 +271,31 @@ document.querySelector('.container-tag').addEventListener('click', (event) => {
   if (event.target.className === 'tag-close-cross') {
     removeTag(tagToRemove);
   }
+});
+
+function filteringArrayDropdownInput(category, targetValue) {
+  const arrayFiltered = objectOfArraysForDropdown[category].filter((el) => {
+    if (el.toLowerCase().search(targetValue) !== -1) {
+      return true;
+    }
+    return false;
+  });
+  return arrayFiltered;
+}
+
+function displayContentOfDropdown(newArrayFiltered, nodeListOfElementInDropdown) {
+  nodeListOfElementInDropdown.forEach((el) => el.dataset.hidden = true);
+  newArrayFiltered.forEach((valueFiltered) => {
+    document.querySelector(`[data-content="${valueFiltered.toLowerCase()}"]`).dataset.hidden = false;
+  });
+}
+
+document.querySelector('.container-dropdown').addEventListener('input', (event) => {
+  const category = event.target.getAttribute('value').toLowerCase(); // ingredient, appareils, ustensils
+  const targetValue = event.target.value.toLowerCase();
+  const nodeListOfElementInDropdown = document.querySelectorAll(`[data-category="${category}"]`);
+
+  const newArrayFiltered = filteringArrayDropdownInput(category, targetValue);
+
+  displayContentOfDropdown(newArrayFiltered, nodeListOfElementInDropdown, category);
 });
